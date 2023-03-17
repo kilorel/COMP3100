4e850be43b9470a87782b7938233c889dd67923d
kilorel <kilo1210@gmail.com>
2023-03-17 13:06:46 +1100

    edited TCPClient to work with ds-server

--- TCPClient.java
+++ TCPClient.java
@@ -1,41 +1,63 @@
 import java.net.Socket;
 import java.net.InetAddress;
-import java.io.DataInputStream;
+import java.io.BufferedReader;
+//import java.io.DataInputStream;
+import java.io.InputStreamReader;
+//import java.io.OutputStreamWriter;
 import java.io.DataOutputStream;
-import java.util.concurrent.TimeUnit;
+//import java.util.concurrent.TimeUnit;
 
 public class TCPClient {
 	public static void main(String[] args){
-		while (true) {
+
 			try{
 			    InetAddress aHost = InetAddress.getByName(args[0]);
 			    int aPort = Integer.parseInt(args[1]);
 			    Socket s = new Socket(aHost,aPort);
+				//Socket s = new Socket("127.0.0.1",50000);
+				//DataInputStream din = new DataInputStream(s.getInputStream());
 			    DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-			    DataInputStream din = new DataInputStream(s.getInputStream());
+			    BufferedReader din = new BufferedReader(new InputStreamReader(s.getInputStream()));
+				String username = System.getProperty("user.name");
 			    
 			    System.out.println("Target IP: " + s.getInetAddress() + " Target Port: " + s.getPort());
 			    System.out.println("Local IP: " + s.getLocalAddress() + " Local Port: " + s.getLocalPort());	
-			    try {TimeUnit.SECONDS.sleep(10);} catch(InterruptedException e){System.out.println(e);}
 			    
-			    dout.writeUTF("HELO");
+			    dout.write(("HELO\n").getBytes());
+				dout.flush();
 			    System.out.println("SENT: HELO");
 			    
-			    String str = (String)din.readUTF();
+			    String str = (String)din.readLine();
 			    System.out.println("RCVD: " +str);
-			    dout.writeUTF("BYE");
-			    System.out.println("SENT: BYE");
-			    
-			    str = (String)din.readUTF();
+
+				dout.write(("AUTH "+username+"\n").getBytes());
+				dout.flush();
+				System.out.println("SENT:AUTH "+username);
+
+				str = (String)din.readLine();
 			    System.out.println("RCVD: "+str);
+
+				dout.write(("REDY\n").getBytes());
+				dout.flush();
+			    System.out.println("SENT: REDY");
+
+				str = (String)din.readLine();
+			    System.out.println("RCVD: "+str);
+
+				dout.write(("QUIT\n").getBytes());
+				dout.flush();
+			    System.out.println("SENT: QUIT");
+
+				str = (String)din.readLine();
+			    System.out.println("RCVD: "+str);			    
+
 			    
 			    din.close();
 			    dout.close();
 			    s.close();
 			}
 			catch(Exception e){System.out.println(e);}
-			try {TimeUnit.SECONDS.sleep(1);} catch(InterruptedException e){System.out.println(e);}
-		}
+		
 	}
 }
 
