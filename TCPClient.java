df7be7233c841b1edc16c69469b6319d733e43a9
kilorel <kilo1210@gmail.com>
2023-05-29 20:58:23 +1000

    edits to code clarity

--- TCPClient.java
+++ TCPClient.java
@@ -2,7 +2,6 @@ import java.net.Socket;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.DataOutputStream;
-import java.util.ArrayList;
 
 public class TCPClient {
 
@@ -10,9 +9,9 @@ public class TCPClient {
 		try {
 			dout.write((input + "\n").getBytes()); // Send message to server
 			dout.flush(); // flush forces any buffered output bytes to be written out to the stream
-			System.out.println("SENT: " + input);
+			//System.out.println("SENT: " + input);
 			String received = (String) din.readLine(); // recieve message from server
-			System.out.println("RCVD: " + received);
+			//System.out.println("RCVD: " + received);
 			return received; // return for use by string variable rec
 		} catch (Exception e) {
 			System.out.println(e);
@@ -22,19 +21,16 @@ public class TCPClient {
 
 	public static void main(String[] args) {
 		try {
-			// InetAddress aHost = InetAddress.getByName(args[0]); //Get IP Address
-			// int aPort = Integer.parseInt(args[1]); //Get Port
-			// Socket s = new Socket(aHost,aPort); //Create Socket Using Inputs
 			Socket s = new Socket("127.0.0.1", 50000); // Socket with set IP Address and Port Number
 			DataOutputStream dout = new DataOutputStream(s.getOutputStream()); // Stream to send messages to server
-			BufferedReader din = new BufferedReader(new InputStreamReader(s.getInputStream())); // Stream to receive
-																								// messages from server
+			BufferedReader din = new BufferedReader(new InputStreamReader(s.getInputStream())); // Stream to receive messages from server
 			String username = System.getProperty("user.name"); // Username grabbed from system
 			String[] jobInfo;
 			String[] dataInfo;
 			String[][] serverInfo;
 			String rec = ""; // received String for use outside of instruct function
-			int serverCount = 0;
+			boolean running = false;
+			boolean checked = false;
 
 			instruct("HELO", dout, din); // Send HELO, Receive OK
 
@@ -54,18 +50,16 @@ public class TCPClient {
 					// Read all lines recieved from data
 					dout.write(("OK\n").getBytes());
 					dout.flush();
-					System.out.println("SENT: OK");
+					//System.out.println("SENT: OK");
 					for (int i = 0; i < serverInfo.length; i++) {
 						rec = (String) din.readLine();
-						System.out.println("RCVD: " + rec);
+						//System.out.println("RCVD: " + rec);
 						serverInfo[i] = rec.split(" ");
 					}
 
 					rec = instruct("OK", dout, din); // Send OK, Receive .
 
-					// Schedules job in first available server
-					boolean running = false;
-					boolean checked = false;
+					// Schedules job in first available server that has no running jobs
 					for (int i = 0; i < serverInfo.length; i++) {
 						if (serverInfo[i][8].equals("0")) {
 							instruct("SCHD " + jobInfo[2] + " " + serverInfo[i][0] + " " + serverInfo[i][1], dout, din);
@@ -73,7 +67,7 @@ public class TCPClient {
 							break;
 						}
 					}
-					
+					// Schedules job in first available server that has no waiting jobs
 					if (running == false){
 						for (int i = 0; i < serverInfo.length; i++) {
 							if (serverInfo[i][7].equals("0")) {
@@ -82,7 +76,8 @@ public class TCPClient {
 								break;
 							}
 						}
-						}
+					}
+					// Schedules job in the first capable server
 					if (running == false && checked == false){
 						instruct("SCHD " + jobInfo[2] + " " + serverInfo[0][0] + " " + serverInfo[0][1], dout, din);
 					}
